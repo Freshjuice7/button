71f234b4e2f463648b849a14492a3f0b3e41dcf0
gzak <vasvac21@gasos-ro.cz>
2025-09-05 09:33:34 +0200

    neco2

--- src/main/java/com/example/firstjavafx/ClickMe.java
+++ src/main/java/com/example/firstjavafx/ClickMe.java
@@ -10,16 +10,43 @@ public class ClickMe  extends Application {
     Button tlacitko;
 
 
+
     @Override
     public void start(Stage primaryStage){
         tlacitko = new Button();
-        tlacitko.setText("ClickMe");
+        tlacitko.setText("PAGE_START");
         BorderPane pane = new BorderPane();
+        pane.setTop(tlacitko);
+
+        tlacitko = new Button();
+        tlacitko.setText("LINE_START");
+        pane.setLeft(tlacitko);
+
+        tlacitko = new Button();
+        tlacitko.setText("CENTER");
         pane.setCenter(tlacitko);
+
+        tlacitko = new Button();
+        tlacitko.setText("LINE_END");
+        pane.setRight(tlacitko);
+
+        tlacitko = new Button();
+        tlacitko.setText("PAGE_END");
+        pane.setBottom(tlacitko);
+
+
+
+
+
+
 
         Scene scene = new Scene(pane,300,200);
         primaryStage.setScene(scene);
         primaryStage.show();
+
+
+
+
 
     }
     public static void main(String[] args) {
